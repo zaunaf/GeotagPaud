1e5e29e917fdf427a771a8e34803059d178ffd6a
Haikal Firdaus <haikal.firdaus.m@gmail.com>
2019-10-09 09:26:26 +0700

    emptystate view

--- app/src/main/java/com/nufaza/geotagpaud/SplashActivity.java
+++ app/src/main/java/com/nufaza/geotagpaud/SplashActivity.java
@@ -6,12 +6,18 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.main_splash);
 
         new Handler().postDelayed(new Runnable() {
