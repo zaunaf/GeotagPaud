234fbc477a974d09f226be9eff2f4f73bdd54009
Iqbal_Mubarok <muhammadiqbalmubarok@gmail.com>
2019-10-16 22:02:37 +0700

    send data is done close#8

--- app/src/main/java/com/nufaza/geotagpaud/managers/DataTransportManager.java
+++ app/src/main/java/com/nufaza/geotagpaud/managers/DataTransportManager.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.widget.Toast;
 
 import com.nufaza.geotagpaud.MainActivity;
+import com.nufaza.geotagpaud.model.Foto;
+import com.nufaza.geotagpaud.model.Foto_Table;
 import com.nufaza.geotagpaud.model.Geotag;
 import com.nufaza.geotagpaud.model.Geotag_Table;
 import com.nufaza.geotagpaud.util.HttpCallback;
@@ -19,7 +21,7 @@ import okhttp3.Response;
 
 public class DataTransportManager {
 
-    public static void sendData(final MainActivity context){
+    public static void sendData(final MainActivity context) {
 
         final List<Geotag> geotags = SQLite.select().from(Geotag.class).where(Geotag_Table.status_data.lessThanOrEq(1)).queryList();
 
@@ -63,6 +65,51 @@ public class DataTransportManager {
                 }
             });
         }
+    }
 
+    public static void sendDataFoto(final MainActivity context) {
+
+        final List<Foto> fotos = SQLite.select().from(Foto.class).where(Foto_Table.status_data.eq(1)).queryList();
+
+        if (fotos.size() > 0) {
+            for (int i = 0; i < fotos.size(); i++) {
+                final Foto foto = fotos.get(i);
+                JSONObject jsonObject = foto.getJSONObject();
+
+                HttpCaller hc = new HttpCaller(
+                        context,
+                        HttpCaller.POST,
+                        "/api/fotos",
+                        null,
+                        jsonObject,
+                        HttpCaller.RETURN_TYPE_JSON,
+                        new HttpCallback() {
+                            @Override
+                            public void onSuccess(JSONObject responseJSO, Response response) {
+                                // System.out.println(responseJSO.toString());
+
+                                if (response.code() == 201) {
+
+                                    foto.setStatusData(2);
+                                    foto.save();
+                                    context.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            Toast.makeText(context.getApplicationContext(), "Foto ID:" + foto.getFotoId() + " terkirim..", Toast.LENGTH_LONG).show();
+                                        }
+                                    });
+                                }
+                            }
+                        });
+
+            }
+        } else {
+            context.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(context.getApplicationContext(), "Data Foto sudah terkirim..", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 }
